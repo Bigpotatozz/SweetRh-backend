aca072e9280b2abc0ea541459b43f64e3b9ea8f6
oscar <oscarmancilla227@gmail.com>
2026-01-05 14:45:44 -0600

    add:security issues

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,4 +1,14 @@
 import { Sequelize } from 'sequelize-typescript';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const credenciales = {
+  host: process.env.HOST,
+  port: parseInt(process.env.PORT as string, 10),
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+};
 
 export const databaseProviders = [
   {
@@ -6,14 +16,13 @@ export const databaseProviders = [
     useFactory: async () => {
       const sequelize = new Sequelize({
         dialect: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: 'root',
-        database: 'nestjs',
+        host: credenciales.host,
+        port: credenciales.port,
+        username: credenciales.username,
+        password: credenciales.password,
+        database: credenciales.database,
       });
       sequelize.addModels([]);
-
       await sequelize.sync();
       return sequelize;
     },
